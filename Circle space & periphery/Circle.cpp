7a082860ac922677bc35062500100f64ea603b44
Soheil <sohail_nader@yahoo.com>
2016-09-18 02:37:20 +0430

    Added more C++ projects
    A C++ program that can calculate space and periphery of circle by entering length of radius.

---
+++ Circle space & periphery/Circle.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+double space(int x);
+double periphery(int x);
+
+int main(){
+	int y;
+	cout<<"Enter the radius of circle: ";
+	cin>>y;
+	cout<<"space: "<<space(y)<<endl;
+	cout<<"periphery: "<<periphery(y)<<endl;
+	cin>>y;
+	getchar();
+	return 0;
+}
+double space(int x){
+	double a;
+	a=x*x*3.14;
+	return a;
+}
+double periphery(int x){
+	double b;
+	
+	b=2*x*3.14;
+	return b;
+}
